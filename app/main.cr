6383ffa797355991c73ea4f265bef36a9ec294eb
Oblivious <thanos.papap42@gmail.com>
2024-03-21 21:20:55 +0200

    Connecting to TCP server.

--- app/main.cr
+++ app/main.cr
@@ -1,17 +1,15 @@
-require "socket"
+require "socket";
 
 # Ensure that the program terminates on SIGTERM, https://github.com/crystal-lang/crystal/issues/8687
-Signal::TERM.trap { exit }
+Signal::TERM.trap { exit; };
 
 class YourRedisServer
   def start
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    puts("Logs from your program will appear here!")
+    puts "Logs from your program will appear here!";
 
-    # Uncomment this block to pass the first stage
-    # server = TCPServer.new("0.0.0.0", 6379)
-    # client = server.accept?
+    server = TCPServer.new "0.0.0.0", 6379;
+    client = server.accept?;
   end
 end
 
-YourRedisServer.new.start
+YourRedisServer.new.start;
