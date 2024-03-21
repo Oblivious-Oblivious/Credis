dbe32af268cc78fb1da597e070ebd55114d85989
Oblivious <thanos.papap42@gmail.com>
2024-03-21 22:37:47 +0200

    Abstracted behaviour.

--- app/main.cr
+++ app/main.cr
@@ -3,12 +3,33 @@ require "socket";
 # Ensure that the program terminates on SIGTERM, https://github.com/crystal-lang/crystal/issues/8687
 Signal::TERM.trap { exit; };
 
-class YourRedisServer
-  def start
-    puts "Logs from your program will appear here!";
+class Commands
+  getter :client;
 
+  def initialize(@client : TCPSocket) end
+
+end
+
+class YourRedisServer
+  private def setup_client
     server = TCPServer.new "0.0.0.0", 6379;
     client = server.accept?;
+  end
+
+  private def generate_commands_from(client : TCPSocket | Nil)
+    begin
+      client = client.not_nil!;
+      cmd = Commands.new client;
+    rescue NilAssertionError
+      exit;
+    end
+  end
+
+  def start
+    server = TCPServer.new "0.0.0.0", 6379;
+    client = server.accept?;
+    cmd = generate_commands_from client;
+
   end
 end
 
