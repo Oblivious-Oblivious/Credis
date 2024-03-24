3d0a8fd20064217365eca59b611c69945d8d5a35
Oblivious <thanos.papap42@gmail.com>
2024-03-25 01:32:56 +0200

    Added functionality for multiple ports.

--- app/main.cr
+++ app/main.cr
@@ -4,4 +4,13 @@ require "./RedisServer";
 # Ensure that the program terminates on SIGTERM, https://github.com/crystal-lang/crystal/issues/8687
 Signal::TERM.trap { exit; };
 
-(RedisServer.new "0.0.0.0", 6379).start;
+def handle_port_number(port_str)
+  port_str.to_i;
+end
+
+if ARGV.size < 2
+  (RedisServer.new "0.0.0.0", 6379).start;
+else
+  port = handle_port_number ARGV[1];
+  (RedisServer.new "0.0.0.0", port).start;
+end
