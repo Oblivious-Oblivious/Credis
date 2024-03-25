5736c09cd0e8a353650d0572a84fb7fb39215573
Oblivious <thanos.papap42@gmail.com>
2024-03-25 20:00:13 +0200

    Second step of master-slave handshake (replconf).

--- app/RedisServer.cr
+++ app/RedisServer.cr
@@ -24,19 +24,41 @@ class RedisServer < TCPServer
     end
   end
 
-  private def connect_and_send_handshake_to_master
-    if Redis::ARGS[:host_type] == "slave"
-      host = Redis::ARGS[:master_host];
-      port = Redis::ARGS[:master_port];
-      master_socket = RedisSocket.new host, port;
-      master_socket << encode_array ["ping"];
-      master_socket.close;
+  private def send_replconf_capa_psync2(master_socket)
+    master_socket << encode_array ["replconf", "capa", "psync2"];
+  end
+
+  private def send_replconf_port(master_socket)
+    port = Redis::ARGS[:port];
+    master_socket << encode_array ["replconf", "listening-port", port];
+    master_socket.flush;
+    if master_socket.gets == encode_simple_string("OK").chomp
+      send_replconf_capa_psync2 master_socket;
     end
+  end
+
+  private def send_ping(master_socket)
+    master_socket << encode_array ["ping"];
+    master_socket.flush;
+    if master_socket.gets == encode_simple_string("PONG").chomp
+      send_replconf_port master_socket;
+    end
+  end
+
+  private def connect_and_send_handshake_to_master
+    host = Redis::ARGS[:master_host];
+    port = Redis::ARGS[:master_port].to_i;
+    master_socket = RedisSocket.new host, port;
+    send_ping master_socket;
+    master_socket.close;
   end
 
   def initialize(host, port)
     super host, port;
-    connect_and_send_handshake_to_master;
+
+    if Redis::ARGS[:host_type] == "slave";
+      connect_and_send_handshake_to_master;
+    end
   end
 
   def start
